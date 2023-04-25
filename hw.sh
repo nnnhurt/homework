b05377b200295e33d5925e647201db6eda3cb5dc
nnnhurt <nnnzagora625@mail.ru>
2023-04-25 14:33:32 +0300

    first commit

---
+++ hw.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+pictures="non-existen_people"
+# Проверяем установлено ли утилита для обработки json из командной строки и устанавливаем если неь
+if ! command -v jq &> /dev/null; then
+    echo "jq is not installed. Installing now..."
+        sudo apt-get update && sudo apt-get install jq
+fi
+# Скачиваем утилиту которая из jpg формата в гиф
+if ! command -v convert &> /dev/null; then
+    echo "imagemagick is not installed. Installing now..."
+        sudo apt-get update && sudo apt-get install imagemagick
+fi
+
+if [[ -d $pictures ]]
+then 
+    echo "Папка для вывода найдена, продолжаем."
+
+else
+    echo "Папка для вывода не найдена, создаем"
+    mkdir -p $pictures
+fi
+
+# Скачиваем 10 чубриков, которая сгенерировала нейросеть
+for i in {0..9}; do
+  # Получаем ответ от сервера Random User API
+  response=$(curl -sS "https://randomuser.me/api/")
+  
+  # Из ответа забираем 10 фоток
+  avatar=$(echo "$response" | jq -r '.results[0].picture.large')
+  
+  # Скачиваем изображения и сохраняем их
+  curl -sS "$avatar" > "$pictures/$i.jpg"
+done
+# Меняем 10 картинок в гифку 
+convert -delay 100 -loop 0 $pictures/* $pictures/users.gif
+
+
+
